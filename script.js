49ab60721346637edb9ae935cfa6253d0b6ba726
Yurii Shevchuk <34710231+YuriShevchuk@users.noreply.github.com>
2021-07-04 19:52:58 +0300

    Add files via upload
    add second canvas

--- script.js
+++ script.js
@@ -2,19 +2,33 @@ const canvas = document.getElementById('cnv1');
 const ctx = canvas.getContext('2d');
 let color = "";
 ctx.rect(0, 0, canvas.width, canvas.height);
-
 const getColor = document.querySelector('#color');
-
+var img = new Image();
+img.src = 'testImg.png';
 
 getColor.addEventListener("click", function () {
-    document.getElementById('color').oninput = function() {
-        var img = new Image();
-        img.src = 'testImg.png';
+     document.getElementById('color').oninput = function() {
         color = this.value;
-        console.log(`in ${color}`)
         ctx.fillStyle = color;
         ctx.fill();
-        ctx.globalCompositeOperation='destination-in';  
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height); 
-    }
+    };
+});
+
+const canvas2 = document.getElementById('cnv2');
+const ctx2 = canvas2.getContext('2d');
+let color2 = "";
+ctx2.rect(0, 0, canvas2.width, canvas2.height);
+const getColor2 = document.querySelector('#color2');
+var img2 = new Image();
+img2.src = 'testImg.png';
+
+
+getColor2.addEventListener("click", function () {
+     document.getElementById('color2').oninput = function() {
+        color2 = this.value;
+        ctx2.fillStyle = color2;
+        ctx2.fill();
+        ctx2.drawImage(img2, 0, 0, canvas2.width, canvas2.height); 
+    };
 });
